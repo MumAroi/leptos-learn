855b4cadc8a62f53dfc5f006360b738a7962fb06
paramas-livinginsider <paramas.w@livinginsider.com>
2024-11-04 11:54:33 +0700

    🚀 deploy: first

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    mode: "all",
+    content: [
+      "./src/**/*.rs",
+      "./index.html",
+      "./src/**/*.html",
+      "./src/**/*.css",
+    ],
+  },
+  theme: {
+    extend: {
+      colors: {
+        "mycolor-1": "#F94892",
+        "mycolor-2": "#FF7F3F",
+        "mycolor-3": "#FBDF07",
+        "mycolor-4": "#89CFFD",
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    require("daisyui")
+  ],
+}
+
